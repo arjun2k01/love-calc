0e68fe082440f6efdab910cc18414cc3ad174ca6
ArjunOG <122463451+arjun2k01@users.noreply.github.com>
2024-08-26 20:48:42 +0530

    Add files via upload

---
+++ LoveClaculator/script.js
@@ -0,0 +1,21 @@
+let calculateLove = (event) => {
+    event.preventDefault();
+    const name1 = document.getElementById('name1').value.trim()
+    const name2 = document.getElementById('name2').value.trim()
+    if (name1 === '' || name2 === '') {
+        alert('Please Enter Both Names');
+        return;
+    }
+    const lovePercentage = Math.floor(Math.random() * 101);
+    const result = document.getElementById('result');
+    result.innerHTML = `${name1} and ${name2}'s Love Percentage: ${lovePercentage}%`
+    
+    if (lovePercentage < 30) {
+        result.innerHTML += '<br>Not a great match. Keep looking';
+    } else if (lovePercentage >= 30 && lovePercentage < 70) {
+        result.innerHTML += '<br>Potential is higher. Must give it a try.';
+    } else {
+        result.innerHTML+= '<br>Perfect match. Enjoy Love!!!!'
+    }
+
+}
